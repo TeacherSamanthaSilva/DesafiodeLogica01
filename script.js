0f042b391b08ee197b87943e07ded4cc2ebb4169
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-16 22:44:27 -0300

    fim da tarefa
    Nesse commit finalizei a tarefa do vai na web

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Samantha"
+console.log(nome)
+let numero = 1993
+console.log(numero)
+let anoatual = 2022
+let idade = anoatual - numero
+console.log(idade)
+console.log("Estou mexendo no console")
+let quartaFeira = true
+console.log(quartaFeira)
